ecb3eb1821560ff7e64ecb29009ab67e6acf08c3
Sigmund Freud <126617855+kolen44@users.noreply.github.com>
2025-01-20 02:54:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    cors: true, // Разрешить CORS
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Разрешить запросы с любого домена
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization',
+    },
+  },
+});
